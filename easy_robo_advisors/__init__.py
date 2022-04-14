425c8542de9af2bf68c7c2c5f6dcd4eac895a47a
Yoiduki <90896806+y-muen@users.noreply.github.com>
2022-04-14 22:33:31 +0900

    poertry new

---
+++ easy_robo_advisors/__init__.py
@@ -0,0 +1,2 @@
+"""Easy Robo-Advisors."""
+__version__ = '0.1.0'
---
+++ tests/test_easy_robo_advisors.py
@@ -0,0 +1,7 @@
+"""Test version."""
+from assets_checker import __version__
+
+
+def test_version():
+    """Test version."""
+    assert __version__ == '0.1.0'
